88ba1f254cf21b3141cc54a6bd72bccf7df806e4
jeonga-Son <jjeong0106@gmail.com>
2022-10-03 12:29:51 -0400

    Initial Commit

---
+++ wall-challenge/wall_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  wall_challengeApp.swift
+//  wall-challenge
+//
+//  Created by jeonga Son on 2022/10/03.
+//
+
+import SwiftUI
+
+@main
+struct wall_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
